83b785d9ec8d18f79c062a6d3816d09ee53151f8
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-10 08:30:55 -0600

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Ashlyn Barrera on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
